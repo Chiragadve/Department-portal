a0b3c44d3db937e1faab28b75b69b2522e4681f0
Chirag <chiragadve4@gmail.com>
2024-04-15 22:29:45 +0530

    defaulter List commit

--- loginpage/admin.php
+++ loginpage/admin.php
@@ -139,8 +139,25 @@ document.getElementById("mseexecuteButton").addEventListener("click", function()
     xhr.open("GET", "auto-mse-marks-add.php", true);
     xhr.send();
 });
+
+
 </script>
 
+<script>
+document.getElementById("mseexecuteButton").addEventListener("click", function() {
+    // AJAX request to execute SQL queries
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            alert("SQL queries executed successfully.");
+        } else if (this.readyState == 4) {
+            alert("Error executing SQL queries: " + this.responseText);
+        }
+    };
+    xhr.open("GET", "auto-mse-marks-add.php", true);
+    xhr.send();
+});
+</script>
 </body>
 </html>
 
